48ed652641e5d8b886eb6924bebc4a188143c6ab
david4more <domanov.david@gmail.com>
2025-07-06 12:44:13 +0200

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
-#include <SFML/Graphics.hpp>
+#include "Game.h"
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main() {
+	Game game;
+	while (game.isRunning())
+	{
+		game.update();
+		game.render();
+	}
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
-    }
+	return 69;
 }
